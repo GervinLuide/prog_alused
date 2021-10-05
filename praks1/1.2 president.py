74dd5fa2247e2f6b329c6e0e2a10aee89f45445e
Gervin Luide <gervin.luide@khk.ee>
2021-10-05 13:37:36 +0300

    1.2 President

---
+++ praks1/1.2 president.py
@@ -0,0 +1,7 @@
+# defineerin muutuja ja omistan väärtused
+aasta = 2016
+president = "Kersti Kaljulaid"
+lause_keskosa = ". aastal valiti Eesti presidentiks "
+# muutujate kasutamine
+lause = str(aasta) + lause_keskosa + president
+print(lause)
