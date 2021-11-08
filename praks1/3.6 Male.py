ca478813e57b8c4432919d1dec879e916ee12bba
Gervin Luide <gervin.luide@khk.ee>
2021-11-08 11:19:59 +0200

    3.6 Male

---
+++ praks1/3.6 Male.py
@@ -0,0 +1,7 @@
+täisarv = int(input("Sisestage täisarv: "))
+i = 1
+nisutera = 1
+while i < täisarv:
+    i += 1
+    nisutera *= 2
+print("Nisuteri " + str(täisarv) + ". ruudu eest: " + str(nisutera))
