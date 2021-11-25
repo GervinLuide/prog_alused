fe286568189842a022bd528f14bb529f4ce213ff
Gervin Luide <gervin.luide@khk.ee>
2021-11-25 14:32:19 +0200

    5.6 tahvli juurde

--- praks1/5.6 Tahvli juurde.py
+++ praks1/5.6 Tahvli juurde.py
@@ -1,11 +1,12 @@
 failinimi = input("Sisestage failinimi: ")
 fail = open(failinimi, encoding="UTF-8")
+
 from datetime import *
-kuupäev = int(datetime.now().day)
+kuupäev = (datetime.now().day)
 i = 1
 for nimi in fail:
     if i == kuupäev:
-        print("tahvli juurde tuleb: " + nimi)
+        print("Vastama tuleb: " + nimi)
         break
     else:
         i += 1
