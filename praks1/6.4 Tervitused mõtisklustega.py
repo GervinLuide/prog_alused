31eb98f26aacc70a66419b8b017883d3cb2dda82
Gervin Luide <gervin.luide@khk.ee>
2021-11-25 16:14:30 +0200

    6.4 Tervitused mõtisklustega

--- praks1/6.4 Tervitused mõtisklustega.py
+++ praks1/6.4 Tervitused mõtisklustega.py
@@ -1,9 +1,9 @@
 def tervitus(järjekorranumber):
     i = 1
     while i <= järjekorranumber:
-        print("täna " + str(i) + "kord tervitada, mõiskleb võõrustaja.")
+        print("Võõrustaja: 'Tere!'")
+        print("täna " + str(i) + ". kord tervitada, mõiskleb võõrustaja.")
         print("külaline: " + "'Tere, suur tänu kutse eest!'")
-        print("võõrustaja: 'Tere!'")
         i += 1
 külaliste_arv = int(input("Sisestage külaliste arv: "))
 print(tervitus(külaliste_arv))
