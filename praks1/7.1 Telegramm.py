6c5592d04b7b253eb70613f9fd62a4e7c9d77b33
Gervin Luide <gervin.luide@khk.ee>
2021-11-30 15:25:01 +0200

    7.1 Telegramm

---
+++ praks1/7.1 Telegramm.py
@@ -0,0 +1,6 @@
+failinimi = input("Sisestage failinimi: ")
+fail = open(failinimi, encoding="UTF-8")
+sisu = fail.read()
+fail.close()
+lause = sisu.upper().replace("Ä", "AE").replace("Õ", "OE").replace("Ö", "OE").replace("Ü", "UE")
+print(lause)
