fa38602106985c1c65000efe84c25c95b5eb5729
Gervin Luide <gervin.luide@khk.ee>
2021-11-30 13:32:00 +0200

    6.6 kuupäev

---
+++ praks1/6.6 Kuupäev.py
@@ -0,0 +1,11 @@
+def kuu_nimi(kuu_järjekorranumber):
+    kuud = ["jaanuar", "veebruar", "märts", "april", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"]
+    return kuud[int(kuu_järjekorranumber) -1]
+
+def kuupäev_sõnena(kuupäev):
+    kuupäevad = kuupäev.split(".")
+    kuupäev = kuupäevad[0] + "." + kuu_nimi(kuupäevad[1]) + " " + kuupäevad[2] + ". a"
+    return kuupäev
+
+kuupäev = str(input("Sisestage kuupäev kujul DD.MM.YYYY: "))
+print(str(kuupäev_sõnena(kuupäev)))
